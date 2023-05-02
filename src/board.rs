e9aed4e14cce59a0bddbc808c09b2d308bc27515
Sandra <sandra97termaat@hotmail.nl>
2023-05-02 16:02:58 +0200

    todo move pieces

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_mod_picking::{Hover, PickableBundle, Selection};
 
+use crate::chess::chess::Piece;
+
 pub struct BoardPlugin;
 
 impl Plugin for BoardPlugin {
@@ -62,6 +64,12 @@ struct SelectedSquare {
     entity: Option<Entity>,
 }
 
+#[derive(Default, Resource)]
+struct SelectedPiece {
+    entity: Option<Entity>,
+}
+
+
 fn create_board(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -114,3 +122,49 @@ fn color_squares(
         }
     }
 }
+
+// fn select_square(
+//     mouse_button_inputs: Res<Input<MouseButton>>,
+//     mut selected_square: ResMut<SelectedSquare>,
+//     mut selected_piece: ResMut<SelectedPiece>,
+//     squares_query: Query<&Square>,
+//     mut pieces_query: Query<(Entity, &mut Piece)>,
+// ) {
+//     // Only run if the left button is pressed
+//     if !mouse_button_inputs.just_pressed(MouseButton::Left) {
+//         return;
+//     }
+
+//     // Get the square under the cursor and set it as the selected
+//     if let Some((square_entity, _intersection)) = pick_state.top(Group::default()) {
+//         // Get the actual square. This ensures it exists and is a square
+//         if let Ok(square) = squares_query.get(*square_entity) {
+//             // Mark it as selected
+//             selected_square.entity = Some(*square_entity);
+
+//             if let Some(selected_piece_entity) = selected_piece.entity {
+//                 // Move the selected piece to the selected square
+//                 if let Ok((_piece_entity, mut piece)) = pieces_query.get_mut(selected_piece_entity)
+//                 {
+//                     piece.x = square.x;
+//                     piece.y = square.y;
+//                 }
+//                 selected_square.entity = None;
+//                 selected_piece.entity = None;
+//             } else {
+//                 // Select the piece in the currently selected square
+//                 for (piece_entity, piece) in pieces_query.iter_mut() {
+//                     if piece.x == square.x && piece.y == square.y {
+//                         // piece_entity is now the entity in the same square
+//                         selected_piece.entity = Some(piece_entity);
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//     } else {
+//         // Player clicked outside the board, deselect everything
+//         selected_square.entity = None;
+//         selected_piece.entity = None;
+//     }
+// }
--- src/pieces.rs
+++ src/pieces.rs
@@ -6,7 +6,8 @@ pub struct PiecesPlugin;
 
 impl Plugin for PiecesPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(create_pieces);
+        app.add_startup_system(create_pieces)
+            .add_system(move_pieces);
     }
 }
 
@@ -310,14 +311,14 @@ fn create_pieces(
     }
 }
 
-// fn move_pieces(time: Res<Time>, mut query: Query<(&mut Transform, &Piece)>) {
-//     for (mut transform, piece) in query.iter_mut() {
-//         // Get the direction to move in
-//         let direction = Vec3::new(piece.x as f32, 0., piece.y as f32) - transform.translation;
+fn move_pieces(time: Res<Time>, mut query: Query<(&mut Transform, &Piece)>) {
+    for (mut transform, piece) in query.iter_mut() {
+        // Get the direction to move in
+        // let direction = Vec3::new(piece.x as f32, 0., piece.y as f32) - transform.translation;
 
-//         // Only move if the piece isn't already there (distance is big)
-//         if direction.length() > 0.1 {
-//             transform.translation += direction.normalize() * time.delta_seconds();
-//         }
-//     }
-// }
+        // Only move if the piece isn't already there (distance is big)
+        // if direction.length() > 0.1 {
+        //     transform.translation += direction.normalize() * time.delta_seconds();
+        // }
+    }
+}
