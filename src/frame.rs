16d38e2fc6d2447f646a10a23c96d8dde1999305
Sandra <sandra97termaat@hotmail.nl>
2023-05-26 11:23:18 +0200

    added transforms to moving bar and carrier~

--- src/frame.rs
+++ src/frame.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+// use bevy::prelude::*;
 use bevy::prelude::{shape::Box, *};
 use bevy_rapier3d::prelude::RigidBody;
 
@@ -6,17 +6,23 @@ use crate::board::*;
 
 const BAR_Y: f32 = 0.0;
 const BAR_Z: f32 = 0.0;
-
 const BAR_OFFSET: Vec3 = Vec3::new(
     0.5 * BOARD_WIDTH - 2.5,
     -0.5 * BOARD_HEIGHT,
     0.5 * BOARD_LENGTH - 1.5,
 );
 
-const MAGNET_HEIGHT: f32 = 0.25;
-const MAGNET_RADIUS: f32 = 0.45;
+const CARRIER_HEIGHT: f32 = 0.5;
+const CARRIER_OFFSET: Vec3 = Vec3::new(
+    0.5 * BOARD_WIDTH - 2.5,
+    -0.5 * BOARD_HEIGHT,
+    0.5 * BOARD_LENGTH - 1.5,
+);
+
+const MAGNET_HEIGHT: f32 = 0.5;
+const MAGNET_RADIUS: f32 = 0.25;
 const MAGNET_Y: f32 = -BOARD_HEIGHT - 0.5 * MAGNET_HEIGHT;
-const MAGNET_OFFSET: Vec3 = Vec3::new(0.0, MAGNET_Y, 0.0);
+const MAGNET_OFFSET: Vec3 = Vec3::new(-1.25, MAGNET_Y, -1.25);
 pub struct FramePlugin;
 
 impl Plugin for FramePlugin {
@@ -25,10 +31,8 @@ impl Plugin for FramePlugin {
             .add_startup_system(create_frame)
             .add_startup_system(create_moving_bar)
             .add_startup_system(create_carrier)
-            .add_startup_system(create_magnet)
-            .add_system(move_magnet)
-            .add_system(move_carrier)
-            .add_system(move_bar);
+            .add_startup_system(create_magnet);
+        // .add_system(move_magnet);
     }
 }
 #[derive(Resource)]
@@ -58,7 +62,7 @@ impl FromWorld for FrameColors {
         let carrier = materials.add(Color::rgb(0.5, 0.3, 0.0).into());
 
         let magnet = materials.add(StandardMaterial {
-            base_color: Color::rgb(0.8, 0.7, 1.0),
+            base_color: Color::rgb(0.9, 0.9, 0.9),
             metallic: 1.0,
             ..default()
         });
@@ -107,8 +111,86 @@ fn create_magnet(
         })
         .insert(RigidBody::KinematicPositionBased)
         .insert(Magnet {
+            target_pos: Vec2 {
+                x: MAGNET_OFFSET[0],
+                y: MAGNET_OFFSET[2],
+            },
+        });
+}
+
+fn create_moving_bar(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    colors: Res<FrameColors>,
+) {
+    let mesh = meshes.add(Mesh::from(shape::Box {
+        min_x: -1.5,
+        max_x: -1.0,
+        min_y: -1.25,
+        max_y: -1.0,
+        min_z: -3.5,
+        max_z: 10.5,
+    }));
+    commands
+        .spawn(PbrBundle {
+            mesh,
+            material: colors.bar.clone(),
+            transform: Transform::from_translation(BAR_OFFSET),
+            ..default()
+        })
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Bar { target_pos: 0.0 });
+}
+
+fn create_carrier(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    colors: Res<FrameColors>,
+) {
+    let mesh = meshes.add(Mesh::from(shape::Box {
+        min_x: -1.75,
+        max_x: -0.75,
+        min_y: -1.0,
+        max_y: -0.75,
+        min_z: -1.75,
+        max_z: -0.75,
+    }));
+    commands
+        .spawn(PbrBundle {
+            mesh,
+            material: colors.carrier.clone(),
+            transform: Transform::from_translation(CARRIER_OFFSET),
+            ..default()
+        })
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Carrier {
             target_pos: Vec2 { x: 0.0, y: 0.0 },
         });
+}
+
+fn move_magnet(
+    time: Res<Time>,
+    mut magnet_query: Query<(&mut Transform, &Magnet, Without<Bar>, Without<Carrier>)>,
+    mut bar_query: Query<(&mut Transform, With<Bar>, Without<Magnet>, Without<Carrier>)>,
+    mut carrier_query: Query<(&mut Transform, With<Carrier>, Without<Magnet>, Without<Bar>)>,
+) {
+    let (mut bar_transform, _, _, _) = bar_query.get_single_mut().unwrap();
+    let (mut carrier_transform, _, _, _) = carrier_query.get_single_mut().unwrap();
+    let (mut magnet_transform, _, _, _) = magnet_query.get_single_mut().unwrap();
+
+    bar_transform.translation.x = magnet_transform.translation.x;
+    carrier_transform.translation.x = magnet_transform.translation.x;
+    carrier_transform.translation.y = magnet_transform.translation.y;
+
+    let magnet_direction = Vec3::new(
+        magnet_transform.translation.x,
+        MAGNET_Y,
+        magnet_transform.translation.y,
+    ) - magnet_transform.translation;
+
+    if magnet_direction.length() > 0.05 {
+        magnet_transform.translation += magnet_direction.normalize() * time.delta_seconds();
+    }
 }
 
 fn create_frame(commands: Commands, meshes: ResMut<Assets<Mesh>>, colors: Res<FrameColors>) {
@@ -196,88 +278,5 @@ fn create_part_of_frame(
                 ..default()
             })
             .insert(RigidBody::Fixed);
-    }
-}
-
-fn create_moving_bar(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    colors: Res<FrameColors>,
-) {
-    let mesh = meshes.add(Mesh::from(shape::Box {
-        min_x: 5.75,
-        max_x: 6.25,
-        min_y: -1.25,
-        max_y: -1.0,
-        min_z: -3.5,
-        max_z: 10.5,
-    }));
-    commands
-        .spawn(PbrBundle {
-            mesh,
-            material: colors.bar.clone(),
-            ..default()
-        })
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Bar { target_pos: -5.0 });
-}
-
-fn create_carrier(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    colors: Res<FrameColors>,
-) {
-    let mesh = meshes.add(Mesh::from(shape::Box {
-        min_x: 6.0,
-        max_x: 7.0,
-        min_y: -1.0,
-        max_y: -0.5,
-        min_z: -3.5,
-        max_z: -2.5,
-    }));
-    commands
-        .spawn(PbrBundle {
-            mesh,
-            material: colors.carrier.clone(),
-            ..default()
-        })
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Carrier {
-            target_pos: Vec2 { x: -5.0, y: 3.5 },
-        });
-}
-
-fn move_magnet(
-    time: Res<Time>,
-    mut magnet_query: Query<(&mut Transform, &Magnet)>,
-    mut bar_query: Query<(&mut Transform, With<Bar>)>,
-    mut carrier_query: Query<(&mut Transform, &Carrier)>,
-) {
-
-
-    let (mut bar_transform, _) = bar_query.get_single_mut().unwrap();
-    let (mut carrier_transform, _) = carrier_query.get_single_mut().unwrap();
-
-    let (mut magnet_transform, _) = magnet_query.get_single_mut().unwrap();
-
-    bar_transform.translation.x = magnet_transform.translation.x;
-}
-
-fn move_bar(time: Res<Time>, mut query: Query<(&mut Transform, &Bar)>) {
-    for (mut transform, bar) in query.iter_mut() {
-        let direction = Vec3::new(bar.target_pos, BAR_Y, BAR_Z) - transform.translation;
-        if direction.length() > 0.05 {
-            transform.translation += direction.normalize() * time.delta_seconds();
-        }
-    }
-}
-
-fn move_carrier(time: Res<Time>, mut query: Query<(&mut Transform, &Carrier)>) {
-    for (mut transform, carrier) in query.iter_mut() {
-        let direction =
-            Vec3::new(carrier.target_pos.x, BAR_Y, carrier.target_pos.y) - transform.translation;
-        if direction.length() > 0.05 {
-            transform.translation += direction.normalize() * time.delta_seconds();
-        }
     }
 }
